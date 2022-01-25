76cf44b21b750f14aa13346fdded16711146d6d0
Allana Mendes <allana.mendes@INSOLE.LOCAL>
2022-01-25 13:07:21 -0300

    :construction: changed authenticator

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import './App.css';
 
+import { Amplify } from 'aws-amplify';
+
 import { Authenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
+
+import awsExports from './aws-exports';
+Amplify.configure(awsExports);
 
 function App() {
   return (
     <Authenticator>
       {({ signOut, user }) => (
-        <div className="App">
-          <p>
-            Hey {user.username}, welcome to my channel, with auth!
-          </p>
+        <main>
+          <h1>Hello {user.username}</h1>
           <button onClick={signOut}>Sign out</button>
-        </div>
+        </main>
       )}
     </Authenticator>
   );
 }
-
 export default App;
